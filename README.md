f1dcda913cae30cb508a7693c1759bce161e16c7
Juan <juandedioo_21@alu.uabcs.mx>
2025-08-19 19:03:51 -0700

    yeh

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+# Programacion-movil
+=======
+>>>>>>> 1bc1c30 (yeh)
 # Welcome to your Expo app 👋
 
 This is an [Expo](https://expo.dev) project created with [`create-expo-app`](https://www.npmjs.com/package/create-expo-app).
@@ -48,3 +54,7 @@ Join our community of developers creating universal apps.
 
 - [Expo on GitHub](https://github.com/expo/expo): View our open source platform and contribute.
 - [Discord community](https://chat.expo.dev): Chat with Expo users and ask questions.
+<<<<<<< HEAD
+=======
+>>>>>>> 633ec1a (Initial commit)
+>>>>>>> 1bc1c30 (yeh)
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
-import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>¡Mi primera app con React Native!</Text>
-      <Button
-        title="Presióname"
-        onPress={() => Alert.alert('¡Hola! Este es tu primer botón')}
-      />
+      {/* Input y botón */}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>AÑADIR</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Lista de tareas */}
+      <ScrollView style={styles.tasksContainer}>
+        <View style={styles.task}>
+          <Text style={styles.taskText}>Tarea 1</Text>
+        </View>
+        <View style={styles.task}>
+          <Text style={styles.taskText}>Tarea 2</Text>
+        </View>
+        <View style={styles.task}>
+          <Text style={styles.taskText}>Tarea 3</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -16,13 +33,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20,
+    backgroundColor: 'white',
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
+  inputContainer: {
+    flexDirection: 'row',
     marginBottom: 20,
+  },
+  input: {
+    flex: 1,
+    backgroundColor: 'gray',
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  button: {
+    backgroundColor: 'green',
+    paddingHorizontal: 15,
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  tasksContainer: {
+    flex: 1,
+  },
+  task: {
+    backgroundColor: 'gray',
+    padding: 15,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  taskText: {
+    color: 'white', 
   },
 });
